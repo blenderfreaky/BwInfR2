5ae7b8a0e75c087169f1965e3abe58f9e472934b
blenderfreaky <blendcrafterxiv@gmail.com>
2019-03-10 22:53:12 +0100

    frick

---
+++ Aufgabe2/Aufgabe2_API/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe2_API
+{
+    public struct Triangle
+    {
+        public double[] angles, lenghts;
+    }
+}
---
+++ Aufgabe2/Aufgabe2_API/TriangleArranger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe2_API
+{
+    public static class TriangleArranger
+    {
+    }
+}
